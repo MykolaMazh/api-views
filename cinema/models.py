f27d4967bf2ed7c540ff504f1c18db14203bb140
MykolaMazhula <mykm3ua@gmail.com>
2024-05-02 08:20:36 +0200

    Create Genre, Actor, CinemaHall models and update Movie model

--- cinema/models.py
+++ cinema/models.py
@@ -5,6 +5,32 @@ class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     duration = models.IntegerField()
+    actors = models.ManyToManyField("Actors")
+    genres = models.ManyToManyField("Genres")
 
     def __str__(self):
         return self.title
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return  self.name
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=100)
+    rows = models.PositiveSmallIntegerField()
+    seats_in_row = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return f"Hall '{self.name}'"
